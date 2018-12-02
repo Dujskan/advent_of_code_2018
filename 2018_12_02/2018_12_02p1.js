2ab8779cf1163529179d09aea29b22288e15e846
dujskan <sebbelundin@hotmail.com>
2018-12-02 22:13:22 +0100

    init

---
+++ 2018_12_02/2018_12_02p1.js
@@ -0,0 +1,288 @@
+window.onload = function(){
+    let ids = idList.split('\n');
+    let toSum = [];
+    ids.forEach(id => {
+        toSum.push(getCharOccurence(id));
+    });
+    let twos = 0, threes = 0;
+    toSum.forEach(arrs => {
+        arrs.forEach(num => {
+            if(num == 2) twos++
+            else threes++
+        })
+    });
+
+    console.log(twos * threes);
+};
+
+let getCharOccurence = function(id){
+    let charCount = [];
+    for(let i = 0; i < id.length; i++)
+        if(timesAppearsInArray(id, id[i]) === 2 && charCount.indexOf(2) === -1 
+        ||timesAppearsInArray(id, id[i]) === 3 && charCount.indexOf(3) === -1)
+            charCount.push(timesAppearsInArray(id, id[i]));
+                
+    return charCount;
+}
+
+function timesAppearsInArray(array, elem) {
+    var count = 0;
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === elem) {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+let idList = `dghfbsyiznoumojleevappwqtr
+dghfbsyiznoumkjljevacpmqer
+dghfbbyizioumkjlxevacpwdtr
+dghfbsesznoumkjlxevacpwqkr
+dghfbsynznoumkjlxziacpwqtr
+cghfbsyiznjumkjlxevacprqtr
+dghfjsyizwoumkjlxevactwqtr
+dghfdsyfinoumkjlxevacpwqtr
+hghfbsyiznoumkjlxivacpwqtj
+dgcfbsyiznoumkjlxevacbuqtr
+dghfbsyiznoymnjlxevacpwvtr
+dfhfbsyiznoumkulxevacptqtr
+dghfasyiznovmkjlxevacpwqnr
+dghfbsyihnouikjlxevackwqtr
+dghfbayiznolmkjlyevacpwqtr
+jghfbsyiznoumnjldevacpwqtr
+dhhfbsyuznoumkjlxevakpwqtr
+nehfrsyiznoumkjlxevacpwqtr
+dghfbsyiznxdmkolxevacpwqtr
+dgpfbsyizwlumkjlxevacpwqtr
+yghfbsyiznoumkjlsevacpwqtm
+dghfssyiznoumkjlxevvcpwqjr
+dahfbsyiznoumkjlfevacpwqto
+duhfcsyiznouvkjlxevacpwqtr
+dghfbvyiznoumkjlrevacpwvtr
+dghfgsyiznoumknlxgvacpwqtr
+jghfbeyiznkumkjlxevacpwqtr
+daofbsyiznoumkjlxevampwqtr
+dghfbsyiznojmkjlxeracpcqtr
+dghnbsyiznouokjlxevaclwqtr
+dgifbsyiznoumkjlxevnspwqtr
+dgkfpsziznoumkjlxevacpwqtr
+dghfxsyijnoumkjlxevaccwqtr
+dghfbsyiznolmkjlwevzcpwqtr
+dkhfbsaiznoumkjlxevacpwqtg
+dghfbsygknoumkjlaevacpwqtr
+dghfbsyizizumkjlxevacpxqtr
+ighfbbyijnoumxjlxevacpwqtr
+dghfbsyizrouekjlxevacpwktr
+dghobsyiznoujkjlxevacnwqtr
+dghpbsyizyoumkjlxeaacpwqtr
+dghffsyiznoymkjlxevacewqtr
+dghkbssiznoumzjlxevacpwqtr
+dghfbsyawnoumkjlxevacpwjtr
+drhfbsyiznormkjlfevacpwqtr
+dghfbsoiznouwkjlxevacpwqtn
+dghfmsyiznoumkjlxlvecpwqtr
+dxhfbsyiznoumkjlxeeacvwqtr
+dghnbsyiznoumkjsxevacpwqur
+dghfbsyiznrujkjlxevacpwqtc
+dghfbstoznoumhjlxevacpwqtr
+dghfboyiznzumkjlvevacpwqtr
+dghfbsyiznjumkjlxevgcpwmtr
+dghfbsnizaoumkjlxevacpwetr
+dghfbsyirnoumkjoxevacplqtr
+dghfbsyiznoumkjlxavvckwqtr
+dghfbjdiznoumkjlxevacpwptr
+dghfbsywznoumkjlxeiacpwqcr
+djhfbsyizuoumkjlxelacpwqtr
+dghffsniznoumkjlxpvacpwqtr
+dghebsyizuoumkjlxevecpwqtr
+rghfbsyiznourkjcxevacpwqtr
+dghfbsyignoumkwlxenacpwqtr
+dghfbsyiznrufkjlxevacpwqth
+dgifbsyiznoumkjlxevacpjqnr
+dghfbsyiznoumkjbxevaxpwqtw
+dsufbsyizncumkjlxevacpwqtr
+dihfbsyiznoumujlxecacpwqtr
+dghfbiyiznoumkjlxevajpwqtn
+dghqbsyixnoumkjlrevacpwqtr
+dghfbsyiznouukjlxeuacpwqtx
+dghfbsyizyoumksfxevacpwqtr
+dghfbsiiznopfkjlxevacpwqtr
+eghfbsyidnoumkjlxexacpwqtr
+dghfbgyiznouwkjlwevacpwqtr
+dghfbsyyznoumkjlxevacwwqtf
+bghfbtypznoumkjlxevacpwqtr
+dghfbsyiznoumtjlxebacpwetr
+dghfbsgiznonmkplxevacpwqtr
+dghfbsyiznoumxjlxevanpwqpr
+dghfbsyiznwumujuxevacpwqtr
+dghxbsyiznoumkjzxevaypwqtr
+dghfbsyhznoumkjlxlvacpiqtr
+dghfbsyiznoumkjlxevzcnwqrr
+dvhfbsyiznoumkjluevacpzqtr
+dghcbsyiznoumkjlxmvacpwetr
+dghfbsyiznohmkjvxbvacpwqtr
+dghfzsyiznouokjlxevacpwqpr
+dghfbsyiznoumkjlxevachtqth
+dghfbsyiznoumkjlxjvacpfutr
+dghfbsyiznoumkjlxevsppwqtt
+dghfusyiznouakhlxevacpwqtr
+dghfbsyizcoumkjlxrvaipwqtr
+dghebsyipnoumfjlxevacpwqtr
+dgdfbsyiznoumkjlwevacpkqtr
+dghfbsyiznoumkjlcffacpwqtr
+dghfbsypznfumkjlxevacpwqar
+dghfbsyiznojmkjlxevgcpkqtr
+dghfbsyiznoumkjlaevlcpwstr
+dgafrsyiunoumkjlxevacpwqtr
+dghfbsyiznouqljlxevacrwqtr
+dyhkbsyiznokmkjlxevacpwqtr
+pghfbsciznoumkjlxevacpwvtr
+dghfbxyiznonmkjllevacpwqtr
+ighfbsyizxoumkjlxevacpzqtr
+dgffbsyoznoumkjlxevacpwqto
+hghfbsyiznoumkjlpevachwqtr
+dlhfosyiznoumkjldevacpwqtr
+dghfbsvizkoumkjlxvvacpwqtr
+dbafbsyiznozmkjlxevacpwqtr
+dghfbsyidnoumkjlxrvjcpwqtr
+dghfbsyiznfumkjlxeqacpwqta
+dghfbsfiznoumkjvxevacjwqtr
+dghfbsyimnoumrjlhevacpwqtr
+dghflsyiznoumkjlxevacpvqmr
+dghfbmfiznoumkjlxevacpdqtr
+dghfbsyizsouzkjlxevscpwqtr
+dghfksyiznoumimlxevacpwqtr
+dghfbsyiznoumkjlxevbwpwqur
+wghcbsyiznoumkjlkevacpwqtr
+kghfbioiznoumkjlxevacpwqtr
+dghfbsiizeoumkjlxmvacpwqtr
+dglfbsyilnoumkjlxevpcpwqtr
+dgqfbsylznoumkjlxevacpwqcr
+dglfhsyiznoumkjlxevacpwqdr
+dghfbsymznoumkjlxbvacpwqtb
+hghfbsyizhoumkjlxtvacpwqtr
+dghdbsyiznoumkjlxeiacpyqtr
+dohfbsyiznoumkjmxlvacpwqtr
+xhhfbsyiznoumkjlxegacpwqtr
+dlhfbsyiznoumkjlxnvahpwqtr
+dghfbsyiznovdpjlxevacpwqtr
+dgcfbsyiznoumkjlxevactwqdr
+dghfksyiknoumkjlxevacpwqcr
+ughfqsyiznoumkjlxevacpwctr
+dghfbjyiznoumkjlxsvacnwqtr
+dgwfbagiznoumkjlxevacpwqtr
+dghfbsyiznoumknlxevtcpwqdr
+jghfksyiznoumkjlxeoacpwqtr
+dghfbsyiznoimkjlwezacpwqtr
+dghfbsyiunoumkjlxeqacpwstr
+dghfbsyizjoumkwlxevaypwqtr
+dghfysriznoumkjlxevucpwqtr
+dghfbsygzjoumkjfxevacpwqtr
+dghfbhviznoumkjlxevacpwqtq
+dghfbsyiznoumkjvwevacpwqur
+dghfbsyiznoumtjlxevacplqnr
+yghfbsysznouykjlxevacpwqtr
+dgwfbsiiznoumkjlxevacfwqtr
+dghfbsyizooumkjlxevampiqtr
+dshfbsyiznoumkjlxevawpoqtr
+dghtbsyxznuumkjlxevacpwqtr
+dkhfblyiznoumkjlxevacpaqtr
+dgkfbsyiinoumkjlxegacpwqtr
+dghfbtxiznouhkjlxevacpwqtr
+dghfbsyiznoumkjlxkvmcpeqtr
+dghfbsyiznoumkjlghvacpwqmr
+dghfbsbizioumkjlcevacpwqtr
+dphfbsyizhoumkjwxevacpwqtr
+dghfbsyiznqumkjlugvacpwqtr
+dghfbsjinnoumkjlxevacpwetr
+mghfbsyiznoumkjlxfvacpjqtr
+dghfbsxiznoumkjlxetacwwqtr
+dghmbsyiznoumbjlxevacpwqyr
+dghfbsyiznwumkjlwevacmwqtr
+dgkfbsyiznotmkjlxevacpwstr
+dghfbsyiznouykjlxeiacuwqtr
+dghfbsynznbhmkjlxevacpwqtr
+dgyfbsyiznoumtjlbevacpwqtr
+dghfbftiznoumkjlxevacpwatr
+dghfvsyiznouikjlievacpwqtr
+dghfbsyiznodmkjlxevncpwqtz
+yfhfbsyiznoumkjluevacpwqtr
+dghfbzyiznoumhflxevacpwqtr
+dphfbsyizncumkjlxevacpwqtf
+dghfasyiznoumkjlxeaicpwqtr
+dgffbsyiznoumkjlzevacpwqsr
+dghfbsyiznoumkmxxcvacpwqtr
+dghffsyiznoumkjlxevacpwqre
+dghfbsyizndmmkjlxemacpwqtr
+dghfbsviznoamkjlxevappwqtr
+dghfbsyiznouckrlxevacpdqtr
+dgwfbsyiznyumkjlxevacpqqtr
+dujfbsyiznoumgjlxevacpwqtr
+dghobsailnoumkjlxevacpwqtr
+dghfkqyiznoumknlxevacpwqtr
+dghfbyypznoumkjlxevacpwatr
+wqhfbsyiznoumkjlxevzcpwqtr
+dghfbsyiznoumwjlxrvacppqtr
+dghfbsymznoumkflxevacplqtr
+dghfbsyiznounkjpgevacpwqtr
+ighfbsyijnoumxjlxevacpwqtr
+dghfbsyizroumkjllevncpwqtr
+dghfbsliznokmkjlxevacpwqtb
+dgefbsyiznoumkqlxevpcpwqtr
+dghfbtypznouzkjlxevacpwqtr
+dmhfbsyiznoumkjlxeyactwqtr
+vohfbsyiznoumkjlqevacpwqtr
+dgsfpsyiznodmkjlxevacpwqtr
+dghfzsyijnoumkjnxevacpwqtr
+dghfbayijroumkjlxevacpwqtr
+dghfbsyiznodmxjlxgvacpwqtr
+dghfbsyiznocmkjlxhvaipwqtr
+dghmbsyignoumkjlxevacpoqtr
+dghfbsyiznosmkjlncvacpwqtr
+dggfbsyiznuumkjlxevacpwqrr
+dghibsyilnoumkjlxevacowqtr
+dghfbsyiznoumkjluevbcowqtr
+dghfbsaiznyuvkjlxevacpwqtr
+dgnfxsyiznommkjlxevacpwqtr
+dghfbnyiznoumkjlsnvacpwqtr
+dghfssiiznoumkjlxavacpwqtr
+dghfbsyizneumajlxevacfwqtr
+dghfbsyiznoumkjlxevycpvptr
+qghfbsyizgoumkjlxevacpwttr
+vghfbsyiznoumkjlievaepwqtr
+dghfbsyiznoumejlxjvacpwdtr
+dghfbsyispoumkjlxevacpwqtg
+duhfbsyizpoumkjlxenacpwqtr
+dghfbsyifnoumkblxevacpnqtr
+bghfbsyxznoumkjleevacpwqtr
+dgtfbsyzpnoumkjlxevacpwqtr
+dghfbsyiznoumkjlsecacpwqth
+dghfqsyiznjumkjlxevawpwqtr
+dgcfbsyizboumkjlxevacqwqtr
+dghfbqyiznoumkjkxevacpwqtj
+dgyfbsyfznoumkjlievacpwqtr
+dghfdsyiznoumkplxevacpwdtr
+dphfbsyuznkumkjlxevacpwqtr
+dghfbsyiznoupkjitevacpwqtr
+dghfisyiznoamkjlxevacpwqwr
+dgufbsyiznoumkjlxivvcpwqtr
+dghfbvyiznoumkjlxevacvwqtz
+dghfbsyiqnxumkjlxbvacpwqtr
+dghubsyiznqumkflxevacpwqtr
+dghfbsyiznzumkjlxevacpdbtr
+dghfbsyiznoumkjlxehacpwwrr
+mghfbsyiznoumkjlxevacpwqbp
+dvhfbryiznoumkclxevacpwqtr
+dghbbsyiznotmkjlxevacpwqhr
+dghfrsyiznoomkjlxevacpwqto
+dghfbkyiznoumkjlxeracpxqtr
+dghfbfyizfoumkjlxevacpwjtr
+dghfbsyizqoulkjlxevacpwqtt
+dghfbsyiwnoumkjlxevacxwgtr
+dghfbsyiznormkjlgxvacpwqtr
+dghybsyizioumkjoxevacpwqtr
+dchfbsyiznoumkjlxyvacpwqtc
+dgyfbsyiznouckjlxewacpwqtr
+dakfbsyeznoumkjlxevacpwqtr`;
